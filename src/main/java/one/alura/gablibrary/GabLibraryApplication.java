11d6338350ac5ee7a4e08f62d5c36cf34b7b7dfe
kingabreel <gabrielyury441@gmail.com>
2024-05-04 20:15:20 -0300

    first commit

---
+++ src/main/java/one/alura/gablibrary/GabLibraryApplication.java
@@ -0,0 +1,13 @@
+package one.alura.gablibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GabLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/one/alura/gablibrary/GabLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package one.alura.gablibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
